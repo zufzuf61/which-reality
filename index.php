f5684b71a8a05c39d970265c83050c6a6af1da1b
zufzuf61 <103180508+zufzuf61@users.noreply.github.com>
2022-04-11 13:51:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 <head>
   <title>Oh geez... What reality am I in?</title>
 </head>
-
-<!-- TODO: Remember to upgrade Azure subscription before trial ends. Company credit card is 1234-5678-1234-5678. -->
   
 <body>
 <h1> Server Information</h1>
